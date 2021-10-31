63610b22c5ba9cca6c1d485e3234c7b20a9dde89
Santos021 <92143917+Santos021@users.noreply.github.com>
2021-10-31 17:24:33 +0530

    .

---
+++ test1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main (){
+    int n;
+    printf("Number between 1 to 20/n");
+   // for (1<n<20))
+    // {
+     
+    // }
+    
+
+
+
+
+
+
+
+    return 0;
+}
---
+++ user1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main (){
+    int a;
+    printf("Enter the value of n : ");
+    scanf("%d",&a);
+    for(a=0; a<=20 ; a++){
+        printf("%d\n",a);
+    }
+    return 0;
+}
